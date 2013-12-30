ce206312e4883826c7d5ae8b45c243cff3be73a8
Alberto <ardlema@gmail.com>
2013-12-30 12:19:46 +0100

    Consumer implemented

--- src/main/scala/MessageConsumer.scala
+++ src/main/scala/MessageConsumer.scala
@@ -1,7 +1,38 @@
 package ardlema.rabbitmqmessageconsumer
 
-object MessageSender {
+import com.rabbitmq.client.{QueueingConsumer, ConnectionFactory}
+
+object MessageConsumer {
   def main(args: Array[String]) {
-    println("hola wapooooo!!!!")
+    val userName = "guest"
+    val password = "guest"
+    val hostName = "localhost"
+    val portNumber = 5672
+    val exchangeName = "testChannel"
+    val routingKey = "routingKey"
+    val factory = new ConnectionFactory()
+    factory.setUsername(userName)
+    factory.setPassword(password)
+    //factory.setVirtualHost(virtualHost)
+    factory.setHost(hostName)
+    factory.setPort(portNumber)
+    val conn = factory.newConnection()
+    println("Connection created!!!!!!")
+
+    val channel = conn.createChannel()
+    channel.exchangeDeclare(exchangeName, "direct", true)
+    val queueName = channel.queueDeclare().getQueue()
+    channel.queueBind(queueName, exchangeName, routingKey)
+
+    println("Waiting for messages...")
+
+    val consumer  = new QueueingConsumer(channel)
+    channel.basicConsume(queueName, true, consumer)
+
+    while (true) {
+      val delivery = consumer.nextDelivery()
+      val message = new String(delivery.getBody())
+      System.out.println(" [x] Received '" + message + "'")
+    }
   }
 }
